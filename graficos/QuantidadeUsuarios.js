30b50eb8fc7a1b964f2ddbc768919f5dff9486c4
joaozada66 <133677947+joaozada66@users.noreply.github.com>
2024-10-09 08:54:01 -0300

    Add files via upload

---
+++ graficos/QuantidadeUsuarios.js
@@ -0,0 +1,18 @@
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys (dados)
+    const quantidadeUsuarios = Object.values (dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y:quantidadeUsuarios,
+            type: 'bar'
+        }
+    ]
+      
+  }
+
+  quantidadeUsuarios()
