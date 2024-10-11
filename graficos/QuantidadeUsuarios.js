fd9aa37e7a4b915d823a5b9fd14c5c952ed7b9c9
joaozada66 <133677947+joaozada66@users.noreply.github.com>
2024-10-11 10:49:34 -0300

    Add files via upload

--- graficos/QuantidadeUsuarios.js
+++ graficos/QuantidadeUsuarios.js
@@ -12,6 +12,12 @@ async function quantidadeUsuarios() {
             type: 'bar'
         }
     ]
+    
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+    
       
   }
 
